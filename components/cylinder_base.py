a2de6a089d693ec55c696c90bee9bb9d3bf40918
Lando <lando.schumpich@gmail.com>
2022-01-25 12:23:03 +0100

    abstract cylinder volume components into base class. Make Screw inherit from CylinderBade

---
+++ components/cylinder_base.py
@@ -0,0 +1,109 @@
+import logging
+import Rhino
+import Rhino.Geometry as rg
+import Rhino.Collections as rc
+import scriptcontext as sc
+from helpers import serde
+from component import Component
+
+PLANE_KEY = "plane"
+RADIUS_KEY = "radius"
+HEIGHT_KEY = "height"
+PARENT_KEY = "parent"
+
+# TODO: Derive from Component
+class CylinderBase(object):
+    """
+    Abstract base class for cylinder components
+    """
+
+    _LAYER_NAME = "CylinderBase"
+    _GEO_LAYER_NAME = "Geometry"
+
+    def __init__(self, plane, radius, height, parent_identifier=None):
+        self.plane = plane
+        self.radius = radius
+        self.height = height
+
+        if parent_identifier:
+            self.parent_identifier = parent_identifier
+
+        self.volume_geometry = self.calculate_rough_volume(plane, radius, height)
+        self.volume_id = None
+
+    @staticmethod
+    def calculate_rough_volume(plane, radius, height):
+        raise NotImplementedError()
+
+    def serialize(self, doc=None):
+        if doc is None:
+            doc = sc.doc
+
+        # get the dowl layer and create attributes from it
+        main_layer_id = serde.add_or_find_layer(self._LAYER_NAME, doc=doc)
+
+        # create a volume layer as a child of the main Screw layer
+        parent = doc.Layers.FindIndex(main_layer_id)
+        volume_layer_id = serde.add_or_find_layer(
+            "{}{}volume".format(self._GEO_LAYER_NAME, serde.SEPERATOR),
+            doc,
+            serde.VOLUME_COLOR,
+            parent,
+        )
+
+        # create rhino attributes for the volume layer
+        attrs = Rhino.DocObjects.ObjectAttributes()
+        attrs.Name = str(self)
+        attrs.LayerIndex = volume_layer_id
+        if self.volume_id:
+            attrs.ObjectId = self.volume_id
+
+        # create an `ArchivableDictionary` to store fields
+        arch_dict = attrs.UserDictionary
+        arch_dict.Set(PLANE_KEY, self.plane)
+        arch_dict.Set(RADIUS_KEY, self.radius)
+        arch_dict.Set(HEIGHT_KEY, self.height)
+        if self.parent_identifier:
+            arch_dict.Set(PARENT_KEY, self.parent_identifier)
+
+        assembly_ids = [
+            serde.serialize_geometry_with_attrs(self.volume_geometry, attrs, doc)
+        ]
+
+        # TODO: Cylinders will serialize multiple times to new geo, that's bad
+        return doc.Groups.Add(assembly_ids)
+
+    @classmethod
+    def deserialize(cls, group_index, doc=None):
+        if doc is None:
+            doc = sc.doc
+
+        # create a new instance of self
+        self = cls.__new__(cls)
+
+        # find the volume object from the group index
+        volume_obj = doc.Groups.GroupMembers(group_index)[0]
+
+        # retrieve arch_dict from volume_id
+        arch_dict = volume_obj.Attributes.UserDictionary
+
+        # restore fields on self
+        self.plane = arch_dict.GetPlane(PLANE_KEY)
+        self.radius = arch_dict.Item[
+            RADIUS_KEY
+        ]  # C# number types don't play nice with python number types
+        self.height = arch_dict.Item[HEIGHT_KEY]  # So get the element directly instead
+        self.volume_id = volume_obj.Id
+        self.volume_geometry = volume_obj.Geometry
+        parent_ident = arch_dict.GetString(PARENT_KEY)
+        if parent_ident:
+            self.parent_identifier = parent_ident
+
+        return self
+
+    def transform(self, xform):
+
+        self.plane.Transform(xform)
+
+        if self.volume_geometry:
+            self.volume_geometry.Transform(xform)
--- components/screw.py
+++ components/screw.py
@@ -2,6 +2,7 @@ import logging
 import Rhino
 import Rhino.Geometry as rg
 import Rhino.Collections as rc
+from components.cylinder_base import CylinderBase
 import scriptcontext as sc
 from helpers import serde
 from component import Component
@@ -24,21 +25,15 @@ class ScrewFactory(object):
 
 
 # TODO: Derive from Component
-class Screw(object):
+class Screw(CylinderBase):
     """
     For now, only MXxX screws are supported
     """
 
+    _LAYER_NAME = "Screws"
+
     def __init__(self, plane, radius, height, parent_identifier=None):
-        self.plane = plane
-        self.radius = radius
-        self.height = height
-
-        if parent_identifier:
-            self.parent_identifier = parent_identifier
-
-        self.volume_geometry = self.calculate_rough_volume(plane, radius, height)
-        self.volume_id = None
+        super(Screw, self).__init__(plane, radius, height, parent_identifier)
 
     def __str__(self):
         return "Screw: M{}x{}".format(int(self.radius), int(self.height))
@@ -78,88 +73,9 @@ class Screw(object):
     def top_circle(self):
         return self.volume_geometry.CircleAt(self.height)
 
-    def serialize(self, doc=None):
-        if doc is None:
-            doc = sc.doc
-
-        # get the dowl layer and create attributes from it
-        main_layer_id = serde.add_or_find_layer(serde.SCREW_LAYER_NAME, doc=doc)
-
-        # create a volume layer as a child of the main Screw layer
-        parent = doc.Layers.FindIndex(main_layer_id)
-        volume_layer_id = serde.add_or_find_layer(
-            "{}{}volume".format(serde.SCREW_LAYER_NAME, serde.SEPERATOR),
-            doc,
-            serde.VOLUME_COLOR,
-            parent,
-        )
-
-        # create rhino attributes for the volume layer
-        attrs = Rhino.DocObjects.ObjectAttributes()
-        attrs.Name = str(self)
-        attrs.LayerIndex = volume_layer_id
-        if self.volume_id:
-            attrs.ObjectId = self.volume_id
-
-        # create an `ArchivableDictionary` to store fields
-        arch_dict = attrs.UserDictionary
-        arch_dict.Set(PLANE_KEY, self.plane)
-        arch_dict.Set(RADIUS_KEY, self.radius)
-        arch_dict.Set(HEIGHT_KEY, self.height)
-        if self.parent_identifier:
-            arch_dict.Set(PARENT_KEY, self.parent_identifier)
-
-        assembly_ids = [
-            serde.serialize_geometry_with_attrs(self.volume_geometry, attrs, doc)
-        ]
-
-        # assembly_ids = [
-        #     doc.Objects.AddBrep(self.volume_geometry.ToBrep(True, True), attrs)
-        # ]
-
-        # TODO: Screws will serialize multiple times to new geo, that's bad
-        return doc.Groups.Add(assembly_ids)
-
-    @classmethod
-    def deserialize(cls, group_index, doc=None):
-        if doc is None:
-            doc = sc.doc
-
-        # create a new instance of self
-        self = cls.__new__(cls)
-
-        # find the volume_id from group_index
-        volume_id = doc.Groups.GroupMembers(group_index)[0].Id
-
-        # retrieve arch_dict from volume_id
-        arch_dict = doc.Objects.FindId(volume_id).Attributes.UserDictionary
-
-        # restore fields on self
-        self.plane = arch_dict.GetPlane(PLANE_KEY)
-        self.radius = arch_dict.Item[
-            RADIUS_KEY
-        ]  # C# number types don't play nice with python number types
-        self.height = arch_dict.Item[HEIGHT_KEY]  # So get the element directly instead
-        self.volume_id = volume_id
-        self.volume_geometry = cls.calculate_rough_volume(
-            self.plane, self.radius, self.height
-        )
-        parent_ident = arch_dict.GetString(PARENT_KEY)
-        if parent_ident:
-            self.parent_identifier = parent_ident
-
-        return self
-
-    def transform(self, xform):
-
-        self.plane.Transform(xform)
-
-        if self.volume_geometry:
-            self.volume_geometry.Transform(xform)
-
 
 if __name__ == "__main__":
-    Screw = ScrewFactory.create_m_screw(rg.Plane.WorldXY, "M10x50")
+    Screw = ScrewFactory.create_m_screw(rg.Plane.WorldXY, "M10x50", "test")
     group_idx = Screw.serialize()
     Screw = Screw.deserialize(group_idx)
 
